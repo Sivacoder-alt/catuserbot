cbaddbc4908a946a1966a24892cdef8d51266392
Sivacoder-alt <sivaram.r2512@gmail.com>
2025-07-01 15:30:07 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,23 +3,23 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = 7937445
+    API_HASH = "61174d3af854af5471aafb7d3b785470"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "arise"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "mongodb+srv://sivaramr2512:attugattu@01@gszbot.mjk1m5w.mongodb.net/?retryWrites=true&w=majority&appName=GSZbot"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "BQB5HaUAOuwgWitqmGGbzHnQzachED0tOxJhirYZlY_3t_2BlgGK6K34L7sBNcX0WR5lMO-pYHzX_VCyK8w3I_Y_RYyTJDHAvkIF2X-852z9xs0VoMg4qa2ZEiHAGG4KlwzqCqnpKUs4-AEDHejA_j9Ym4oo1vFQJvPa11G-vyP3Hug8de_kDZpSn1-O9-zOqRijZQpamIkp8EZN1ddGdfTrRd-6rbbjVyl5XqBp9z8WQ_c--8axPheDKtwM74aeIaBPXFV2_yOeUawGdNMSTbu1T99x27DeJyOjpY5rEvjnfPQQ4mT_hTSZq6hG5BnXtuB5o9wErtKSy5t95zdUSp8vLb0jIQAAAAFa4o7OAA"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "5937515137:AAEeLUkjgJPvAw9OdlNWNgNu7kGxCKv3L90"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = 2051159780
     # command handler
-    COMMAND_HAND_LER = "."
+    COMMAND_HAND_LER = "5819764430"
     # command hanler for sudo
-    SUDO_COMMAND_HAND_LER = "."
+    SUDO_COMMAND_HAND_LER = "5819764430"
     # External plugins repo
-    EXTERNAL_REPO = "https://github.com/TgCatUB/CatPlugins"
+    EXTERNAL_REPO = "https://t.me/AuToPsY_Trauma"
     # if you need badcat plugins set "True"
-    BADCAT = "False"
+    BADCAT = "True"
